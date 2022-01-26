d1de083f6fb0452a03433dcb89b046ed1ba8901c
tkki_iOSX <tkki_iOSX@icloud.com>
2022-01-26 22:05:41 +0900

    20220126a

--- renshu20220124/ContentView.swift
+++ renshu20220124/ContentView.swift
@@ -11,27 +11,57 @@ struct ContentView: View {
     var body: some View {
         VStack{
             HStack{
-                Image(systemName: "house")
+                Image(systemName: "person")
                     .resizable()
-                    .frame(width: 50, height: 50)
+                    .frame(width: 100, height: 100)
                     .foregroundColor(Color.red)
-                Text("HOUSE")
-                    .font(Font.system(size: 10).bold())
+                Image(systemName: "person")
+                    .resizable()
+                    .frame(width: 100, height: 100)
                     .foregroundColor(Color.blue)
+                /*Text("HOUSE")
+                    .font(Font.system(size: 10).bold())
+                    .foregroundColor(Color.blue)*/
             }
-            .frame(width: 100, height: 50, alignment: .center)
+            HStack {
+                Button(action: {
+                                    print("Button")
+                                }) {
+                                    Text("左の人")
+                                        .bold()
+                                        .padding()
+                                        .frame(width: 100, height: 100)
+                                        .foregroundColor(Color.white)
+                                        .background(Color.red)
+                                        .cornerRadius(10)
+                                }
+                Button(action: {
+                                    print("Button")
+                                }) {
+                                    Text("右の人")
+                                        .bold()
+                                        .padding()
+                                        .frame(width: 100, height: 100)
+                                        .foregroundColor(Color.white)
+                                        .background(Color.blue)
+                                        .cornerRadius(10)
+                                }
+            }
+
+
+            /*.frame(width: 100, height: 50, alignment: .center)
             .overlay(
                             RoundedRectangle(cornerRadius: 0)
                                 .stroke(Color.black, lineWidth: 1)
-                        )
-            Text("this is a test program.this is a test program")
+                        )*/
+            /*Text("this is a test program.this is a test program")
                 .font(Font.system(size: 10).bold())
                 .foregroundColor(Color.black)
                 .frame(width: 150, height: 50, alignment: .center)
                 .overlay(
                                 RoundedRectangle(cornerRadius: 0)
                                     .stroke(Color.black, lineWidth: 1)
-                            )
+                            )*/
         }
 
     }
