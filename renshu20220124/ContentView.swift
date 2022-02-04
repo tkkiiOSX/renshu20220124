6cf6a48745a31df7a82eb8a5f454b64a2c0881ad
tkki_iOSX <tkki_iOSX@icloud.com>
2022-02-04 21:54:29 +0900

    ２０２２０１０４

--- renshu20220124/ContentView.swift
+++ renshu20220124/ContentView.swift
@@ -11,81 +11,101 @@ import SwiftUI
 struct ContentView: View {
 
     @State var OnOff = false
-    //@State var RightP = false
+    @State var sugar = 0
+    @State var milk = 0
 
     var body: some View {
-        VStack{
-            HStack{
-                Image(systemName: "car.fill")
-                //Image(systemName: "cup.and.saucer.fill")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(OnOff ? Color.blue :Color.red )
-                /*Image(systemName: "person")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(OnOff ? Color.red : Color.gray)*/
-                    //.foregroundColor(Color.blue)
-                /*Text("HOUSE")
-                    .font(Font.system(size: 10).bold())
-                    .foregroundColor(Color.blue)*/
-            }
-            HStack {
-                Button(action: {
+        VStack {
+            VStack {
+                Image(systemName: "cup.and.saucer.fill")
 
-                    OnOff = false
+                    .resizable()
+                    .frame(width: 120, height: 100)
+                    .foregroundColor(OnOff ? Color.brown :Color.black )
+                
+                HStack {
+                    Stepper(value: $milk, in: 0 ... 5) {
+                        Text("牛乳　\(milk)")
+                    }
+                    ForEach(0 ..< 5, id: \.self) {
+                            index in
+                        VStack {
+                            if index < milk {
+                                Image(systemName: "sun.max")
+                            }
+                            else {
+                                Image(systemName: "moon.fill")
+                            }
+                            Text("\(index)")
+                        }
+
+                    }
+                }
+                HStack {
+                    Stepper(value: $sugar, in: 0 ... 5) {
+                            Text("砂糖　\(sugar)")
+                    }
+                    ForEach(0 ..< 5, id: \.self) {
+                                index in
+                        VStack {
+                            if index < sugar {
+                                Image(systemName: "sun.max")
+                            }
+                            else {
+                                Image(systemName: "moon.fill")
+                            }
+                            Text("\(index)")
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+            /*HStack {
+                Button(action: {
+                    OnOff.toggle()
+                    sugar.toggle()
+
+
                                 }) {
                                     VStack{
-                                        Image(systemName: "car.fill")
-                                            .foregroundColor(Color.red)
-                                        Text("赤")
-                                            .foregroundColor(Color.red)
+                                        Image(systemName: "takeoutbag.and.cup.and.straw.fill")
+                                            .foregroundColor(sugar ? Color.white :Color.black )
+                                        Text("砂糖")
+                                            .foregroundColor(sugar ? Color.white :Color.black )
                                             .bold()
                                             .padding()
                                     }
                                     .frame(width: 100, height: 100)
                                     .foregroundColor(Color.white)
-                                    .background(Color.gray )
+                                    .background(sugar ? Color(white: 0):Color.gray)
                                     .cornerRadius(10)
 
                                 }
                 Button(action: {
+                    OnOff.toggle()
+                    milk.toggle()
 
-                    OnOff = true
                                 }) {
                                     VStack {
-                                        Image(systemName: "car.fill")
-                                            .foregroundColor(Color.blue)
-                                        Text("青")
-                                            .foregroundColor(Color.blue)
+                                        Image(systemName: "takeoutbag.and.cup.and.straw.fill")
+                                            .foregroundColor(milk ? Color.white :Color.black )
+                                        Text("ミルク")
+                                            .foregroundColor(milk ? Color.white :Color.black )
                                             .bold()
                                             .padding()
 
                                     }
                                     .frame(width: 100, height: 100)
                                     .foregroundColor(Color.white)
-                                    .background(Color.gray )
-                                    //.background(Color.blue)
+                                    .background(milk ? Color(white: 0):Color.gray)
                                     .cornerRadius(10)
-                                    }
-
-            }
+                                    }*/
 
 
-            /*.frame(width: 100, height: 50, alignment: .center)
-            .overlay(
-                            RoundedRectangle(cornerRadius: 0)
-                                .stroke(Color.black, lineWidth: 1)
-                        )*/
-            /*Text("this is a test program.this is a test program")
-                .font(Font.system(size: 10).bold())
-                .foregroundColor(Color.black)
-                .frame(width: 150, height: 50, alignment: .center)
-                .overlay(
-                                RoundedRectangle(cornerRadius: 0)
-                                    .stroke(Color.black, lineWidth: 1)
-                            )*/
-        }
+
 
     }
 }
